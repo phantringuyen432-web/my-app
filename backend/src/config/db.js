eb4d2f9ecb7ca2e864d2d26cd8f467d271c04fa6
Phan Tri Nguyen <phantringuyen432@gmail.com>
2026-05-13 14:41:33 +0700

    fix env mysql

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,17 +1,18 @@
-const mysql = require('mysql2');
+const mysql = require("mysql2");
 
 const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'nguyen',
-  password: '12345',
-  database: 'shop_app'
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT
 });
 
 db.connect((err) => {
   if (err) {
-    console.error('Lỗi kết nối DB:', err);
+    console.log("MySQL connection failed:", err);
   } else {
-    console.log('Kết nối MySQL thành công');
+    console.log("MySQL connected");
   }
 });
 
