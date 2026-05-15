d7d971867e9e30f4e3ed25a7d014a7dda46e8446
Phan Tri Nguyen <phantringuyen432@gmail.com>
2026-05-15 09:38:15 +0700

    convert mysql to postgres

--- backend/src/config/mail.js
+++ backend/src/config/mail.js
@@ -1,11 +1,23 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
+
   service: "gmail",
+
   auth: {
-    user: "phantringuyen432@gmail.com",
-    pass: "skmj ehcu dtod fksi"
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS
   }
+
 });
 
+transporter.verify((err, success) => {
+
+  if (err) {
+    console.log("MAIL ERROR:", err);
+  } else {
+    console.log("MAIL READY");
+  }
+
+});
 module.exports = transporter;
