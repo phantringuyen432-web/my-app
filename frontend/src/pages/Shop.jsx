f58823ecd00b3ddc1dce80f6fb1c4dd5617e5bec
Phan Tri Nguyen <phantringuyen432@gmail.com>
2026-05-27 20:27:24 +0700

    update project

--- frontend/src/pages/Shop.jsx
+++ frontend/src/pages/Shop.jsx
@@ -486,13 +486,24 @@ const Shop = () => {
 
         </div>
 
-        {/* ================= LOADING ================= */}
         {loading ? (
-
-          <div className="text-center py-20 text-xl">
-            Đang tải sản phẩm...
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
+            {[...Array(6)].map(
+              (_, index) => (
+                <div
+                  key={index}
+                  className="bg-white rounded-2xl shadow-md overflow-hidden animate-pulse"
+                >
+                  <div className="h-56 bg-gray-300"></div>
+                  <div className="p-4">
+                    <div className="h-6 bg-gray-300 rounded mb-4"></div>
+                    <div className="h-6 w-1/2 bg-gray-300 rounded mb-6"></div>
+                    <div className="h-12 bg-gray-300 rounded-xl"></div>
+                  </div>
+                </div>
+              )
+            )}
           </div>
-
         ) : (
 
           <>
